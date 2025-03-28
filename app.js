c0f8e20bab5028d4dd9fafc39c0fd4f0585a7b66
dalin <dalinvenn@gmail.com>
2025-03-28 07:55:51 +0700

    second commit

--- app.js
+++ app.js
@@ -9,14 +9,19 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json({ limit: "5mb" }));
 
+app.get("/", (req, res) => {
+  res.send("Backend server is running!");
+});
+
 // Connect to MongoDB
-mongoose.connect("mongodb://localhost:27017/signaturesDB")
+mongoose
+  .connect("mongodb://localhost:27017/signaturesDB")
   .then(() => console.log("MongoDB connected successfully"))
-  .catch(err => console.error("MongoDB connection error:", err));
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 app.use("/api", signatureRoutes);
 
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = app;
